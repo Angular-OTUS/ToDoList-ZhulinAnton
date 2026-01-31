d7339283694f08c3d6955c0e397dcf4614c97898
EAZhulin <EAZhulin@oms.beeline.ru>
2026-01-31 21:55:57 +0600

    homework-2

--- todolist-app/src/app/components/to-do-item/to-do-item.ts
+++ todolist-app/src/app/components/to-do-item/to-do-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-item',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './to-do-item.css',
 })
 export class ToDoItem {
+  @Input() taskText: string = '';    
+  @Input() taskId: number = 0;    
   
+  @Output() delete = new EventEmitter<number>();
+
+  onDelete() {
+    this.delete.emit(this.taskId);
+  }
 }
--- todolist-app/src/app/components/to-do-list/to-do-list.ts
+++ todolist-app/src/app/components/to-do-list/to-do-list.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { ToDoItem } from '../to-do-item/to-do-item';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [],
+  imports: [FormsModule, ToDoItem, MatInputModule, MatButtonModule],
   templateUrl: './to-do-list.html',
   styleUrl: './to-do-list.css',
 })
 export class ToDoList {
+  tasks = [
+    { id: 1, text: 'Wake up' },
+    { id: 2, text: 'Wash up' },
+    { id: 3, text: 'Breakfast' }
+  ];
 
+  newTaskText: string = '';
+
+  deleteTask(id: number) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  addTask() {
+    if (this.newTaskText.trim() === '') {
+      return;
+    }
+  
+    let maxId = 0;
+    for (let task of this.tasks) {
+      if (task.id > maxId) {
+        maxId = task.id;
+      }
+    }
+  
+    const newTask = {
+      id: maxId + 1,           
+      text: this.newTaskText.trim() 
+    };
+  
+    this.tasks.push(newTask);
+  
+    this.newTaskText = '';
+  }
 }
